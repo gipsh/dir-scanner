b35ae928f0de62077d45df8f806beffab1668a12
gipsh <gipshm@gmail.com>
2020-01-15 12:35:35 -0300

    added progress bar

--- df.go
+++ df.go
@@ -9,9 +9,17 @@ import (
 	"os"
 	"sync"
 	"time"
+	"io"
+	"bytes"
+	"github.com/cheggaaa/pb/v3"
 )
 
 var END_MESSAGE = "byebyebye"
+
+type Result struct {
+	Code int
+	Word string
+}
 
 func producer(ch chan<- string, wordlist string, workers int) {
 
@@ -34,10 +42,10 @@ func producer(ch chan<- string, wordlist string, workers int) {
 		ch <- END_MESSAGE
 	}
 
-	fmt.Println("producer done")
+	// fmt.Println("producer done")
 }
 
-func consumer(baseurl string, ch <-chan string, id int, wg *sync.WaitGroup) {
+func consumer(baseurl string, ch <-chan string, rc chan<- Result, id int, wg *sync.WaitGroup) {
 
 	defer wg.Done()
 	client := http.Client{
@@ -54,22 +62,52 @@ func consumer(baseurl string, ch <-chan string, id int, wg *sync.WaitGroup) {
 		if err != nil {
 			log.Fatal(err)
 		}
-		//if response.StatusCode != 400 {
-		fmt.Printf("[%d] %d %s\n", id, response.StatusCode, fullurl)
-		//}
+		rc <- Result{response.StatusCode, fullurl}
 	}
 
 	fmt.Printf("Exit worker %d", id)
 
 }
 
+func resultProcessor(rc <-chan Result, words int) {
+	bar := pb.StartNew(words)
+
+	for {
+		result := <-rc
+		bar.Increment()
+		if result.Code == 200 {
+		  fmt.Printf("[%d] %s \n", result.Code, result.Word)
+		}
+	}
+	bar.Finish()
+}
+
+func lineCounter(r io.Reader) (int, error) {
+    buf := make([]byte, 32*1024)
+    count := 0
+    lineSep := []byte{'\n'}
+
+    for {
+        c, err := r.Read(buf)
+        count += bytes.Count(buf[:c], lineSep)
+
+        switch {
+        case err == io.EOF:
+            return count, nil
+
+        case err != nil:
+            return count, err
+        }
+    }
+}
+
 func main() {
 
-	fmt.Println("--[dir-scanner]--")
+	fmt.Println("--[dir-scanner]--\n")
 
 	urlPtr := flag.String("url", "http://example.com:80", "url")
 	wordlistPtr := flag.String("wordlist", "common.txt", "wordlist file")
-	workersPtr := flag.Int("workers", 3, "workers")
+	workersPtr := flag.Int("workers", 8, "workers")
 
 	flag.Parse()
 
@@ -79,16 +117,22 @@ func main() {
 		os.Exit(1)
 	}
 
+	file, _ := os.Open(*wordlistPtr)
+	lines, _ := lineCounter(file)
+
 	fmt.Println("[-] using url: ", *urlPtr)
-	fmt.Println("[-] wordlist: ", *wordlistPtr)
+	fmt.Printf("[-] wordlist: %s [%d words]\n", *wordlistPtr, lines)
 	fmt.Println("[-] workers:", *workersPtr)
 
 	var wg sync.WaitGroup
 	ch := make(chan string, 5)
+	rc := make(chan Result, 5)
+
+	go resultProcessor(rc, lines)
 
 	for i := 0; i < *workersPtr; i++ {
 		wg.Add(1)
-		go consumer(*urlPtr, ch, i, &wg)
+		go consumer(*urlPtr, ch, rc, i, &wg)
 	}
 
 	go producer(ch, *wordlistPtr, *workersPtr)
